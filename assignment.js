a22f2f1e64b5302b4bed07d7a9233cdedb8cf0d1
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-09-11 18:10:12 +0600

    finaly done

--- assignment.js
+++ assignment.js
@@ -1,102 +1,97 @@
 
-function calculateTax(income, expenses) {
-  if(income < 0 || expenses < 0 || income < expenses){
-    return "Invalid Input" 
-  }
-   let difference = income - expenses;
-   let tax = difference * 0.2;
-   return tax;
-}
-let tk =calculateTax(6000, -1500)
-console.log(tk)
+// function calculateTax(income, expenses) {
+//   if(income < 0 || expenses < 0 || income < expenses){
+//     return "Invalid Input" 
+//   }
+//    let difference = income - expenses;
+//    let tax = difference * 0.2;
+//    return tax;
+// }
+// let tk =calculateTax(6000, -1500)
+// console.log(tk)
 
 
 
-function sendNotification(email) {
-    if(email.includes('@') === false){
-        return "Invalid Email"
-    }
-   let split = email.split('@')
-   let firstPart = split[0];
-    let lastPart = split [1]
-    return firstPart + ' sent you an email from ' + lastPart;
-}
-let enterEmail =sendNotification('farhan34@yahoo.com')
-console.log(enterEmail)
+// function sendNotification(email) {
+//     if(email.includes('@') === false){
+//         return "Invalid Email"
+//     }
+//    let split = email.split('@')
+//    let firstPart = split[0];
+//     let lastPart = split [1]
+//     return firstPart + ' sent you an email from ' + lastPart;
+// }
+// let enterEmail =sendNotification('farhan34@yahoo.com')
+// console.log(enterEmail)
 
 
 
 
-function checkDigitsInName(name) {
-  if(typeof name !== 'string'){
-    return "Invalid Input"; 
-  }
-  for(let i of name){
-    if(!isNaN(parseInt(i))){
-      return true;
-    }
+// function checkDigitsInName(name) {
+//   if(typeof name !== 'string'){
+//     return "Invalid Input"; 
+//   }
+//   for(let i of name){
+//     if(!isNaN(parseInt(i))){
+//       return true;
+//     }
     
-  }
-  return false;
-}
-
-let enterName = checkDigitsInName('tajl23')
-console.log(enterName)
-
-
-
-
-function calculateFinalScore(obj) {
- if(typeof obj !== 'object' ||
-   typeof obj.name !== 'string' || 
-   typeof obj.testScore !== 'number' ||
-   typeof obj.schoolGrade !== 'number' ||
-   typeof obj.isFFamily !== 'boolean' ||
-   obj.testScore > 50 ||
-   obj.schoolGrade > 30
-   
-  
-  )
-   {
-  return  "Invalid Input"
- }
- let totalScore = obj.testScore + obj.schoolGrade;
-
- if(obj.isFFamily === true){
-  totalScore += 20;
-  
- }
-  return totalScore >= 80;
-}
-let object = { name: "Rajib", testScore: 15,  schoolGrade: 25, isFFamily : true  }
-let result = calculateFinalScore(object)
-console.log(result)
-
-
-
-
-// function  waitingTime(waitingTimes  , serialNumber) {
-//  if(!Array.isArray(waitingTimes) || typeof serialNumber !== 'number'){
-//   return ' Invalid Input '
-//  }
-//  let sum = 0;
-//  for(let i of waitingTimes){
-//   sum += i;
-//  }
-// //  console.log(sum)
-//  let avarage = sum / waitingTimes.length;
-// //  console.log(avarage)
-
-// let position =(serialNumber-1) - waitingTimes.length  
-// // console.log(position)
-
-// let yourTime = avarage * position;
-// return yourTime;
-
+//   }
+//   return false;
 // }
 
-// let  result = waitingTime([13, 2], 6)
+// let enterName = checkDigitsInName('tajl23')
+// console.log(enterName)
+
+
+
+
+// function calculateFinalScore(obj) {
+//  if(typeof obj !== 'object' ||
+//    typeof obj.name !== 'string' || 
+//    typeof obj.testScore !== 'number' ||
+//    typeof obj.schoolGrade !== 'number' ||
+//    typeof obj.isFFamily !== 'boolean' ||
+//    obj.testScore > 50 ||
+//    obj.schoolGrade > 30
+   
+  
+//   )
+//    {
+//   return  "Invalid Input"
+//  }
+//  let totalScore = obj.testScore + obj.schoolGrade;
+
+//  if(obj.isFFamily === true){
+//   totalScore += 20;
+  
+//  }
+//   return totalScore >= 80;
+// }
+// let object = { name: "Rajib", testScore: 15,  schoolGrade: 25, isFFamily : true  }
+// let result = calculateFinalScore(object)
 // console.log(result)
+
+
+
+
+function  waitingTime(waitingTimes  , serialNumber) {
+ if(!Array.isArray(waitingTimes) || typeof serialNumber !== 'number'){
+  return ' Invalid Input '
+ }
+ let sum = 0;
+ for(let i of waitingTimes){
+  sum += i;
+ }
+ let avarage = parseInt(sum / waitingTimes.length);
+ let position = (serialNumber -1)-waitingTimes.length
+ let needTime = position * avarage
+ return needTime;
+
+}
+
+let  result = waitingTime([7, 8, 3, 4, 5], "9")
+console.log(result)
 
 
 
